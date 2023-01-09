3edfd9c4761fb2f99094449b5d0221da2eeb0a51
favsto-injenia <92993486+favsto-injenia@users.noreply.github.com>
2023-01-09 12:35:36 +0100

    Update deploy-use-existing-project.sh
    project_id= $DEVSHELL_PROJECT_ID

--- deploy-use-existing-project.sh
+++ deploy-use-existing-project.sh
@@ -36,7 +36,7 @@
 project_id="${gcloud config list --format 'value(core.project)' 2>/dev/null}"
 
 # Get the account name who logged in above 
-gcp_account_name=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
+gcp_account_name=$DEVSHELL_PROJECT_ID
 
 # Get the Org Id (needed for org policies and creating the GCP project)
 org_id=$(gcloud organizations list --format="value(name)")
